f6d445961ae0de90115d02e1404548d36d136f54
Pablo Ortolani <pabloortolani@gmail.com>
2019-10-29 16:29:32 -0300

    initial version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const page1 = () => <h1>Page 1</h1>;
+const page2 = () => <h1>Page 2</h1>;
+const usePageApi = () => {
+  const [data, setData] = useState({});
+
+  useEffect(()=> {
+    axios.get('http://www.fanaticby.com/api/index.php')
+    .then(res => {
+      setData(res.data);
+    }).catch(err =>{
+      console.log(err);
+    })
+  }, []);
+
+  return(
+    <div>
+      <h1>API</h1>
+      <pre>{JSON.stringify(data)}</pre>
     </div>
+  )
+
+}
+
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <ul>
+          <li><Link to='/page1'>Page 1</Link></li>
+          <li><Link to='/page2'>Page 2</Link></li>
+          <li><Link to='/rest'>Rest</Link></li>
+        </ul>
+      </div>
+      <Route path='/page1' component={page1} />
+      <Route path='/page2' component={page2} />
+      <Route path='/rest' component={usePageApi} />
+    </Router>
   );
 }
 
